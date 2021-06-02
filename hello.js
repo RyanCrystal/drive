fe1e2c65f4da8e0c71dec94b946c9d99915c60fa
Ryan <slaric2014@gmail.com>
2021-06-02 00:22:29 -0400

    update

--- hello.js
+++ hello.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('try again\n');
+  res.end('lets go!!\n');
 });
 
 server.listen(port, hostname, () => {
