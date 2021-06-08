138a30317c65b164ae2fb0d8b2826d6621946e82
Ryan <slaric2014@gmail.com>
2021-06-07 22:26:01 -0400

    test

--- hello.js
+++ hello.js
@@ -52,7 +52,7 @@ app.route('/').get((req, res) => {
     return res.end();
 });
 
-app.route('/post').get((req, res) => {
+app.route('/upload').get((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<form action="upload" method="post" enctype="multipart/form-data">');
     res.write('<input type="file" name="fileToUpload"><br>');
