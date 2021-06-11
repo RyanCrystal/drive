0b9afd0bfa4b02531e9fa1ff657898b2d4603238
Ryan <slaric2014@gmail.com>
2021-06-11 01:02:41 -0400

    add env

--- hello.js
+++ hello.js
@@ -6,7 +6,6 @@ const mongodb = require('mongodb');
 const { encrypt, decrypt } = require('./crypto');
  
 var env = process.env.NODE_ENV || 'development';
-console.log(env)
 
 const app = express(); // Initialize the express web server
 app.use(busboy({
