b3bb43aeaa165be311054f44034fa82138d56298
Ryan <slaric2014@gmail.com>
2022-02-19 14:05:54 -0500

    test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');               // Used for manipulation with path
 const fs = require('fs-extra');             // Classic fs
 const mongodb = require('mongodb');
 const { encrypt, decrypt } = require('./crypto');
-const basicAuth = require('express-basic-auth');
+// const basicAuth = require('express-basic-auth');
 // const mongoose = require("./database");
 
 const app = express(); // Initialize the express web server
@@ -14,7 +14,6 @@ const app = express(); // Initialize the express web server
 //     users: { 'ryanskydrive': 'fldfh' },
 //     challenge: true
 // }))
-
 var env = process.env.NODE_ENV || 'development';
 
 app.use(busboy({
